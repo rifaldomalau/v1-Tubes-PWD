21b7b5df6fc5d4ef0be42c6ddfa69bcbefb9bc8f
joycelyn-h <joycelyn452@gmail.com>
2025-12-05 03:30:02 +0700

    Update login.js

--- auth/js/login.js
+++ auth/js/login.js
@@ -1,1 +1,23 @@
 // buat halaman login interaktif
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (e) {
+        const username = form.username.value.trim();
+        const password = form.password.value.trim();
+
+        //validasi input kosong
+        if (username === "" || password === "") {
+            e.preventDefault();
+            alert("Username dan password tidak boleh kosong!");
+            return;
+        }
+
+        //validasi panjang pw
+        if (password.length < 4) {
+            e.preventDefault();
+            alert("Password harus minimal 4 karakter!");
+            return;
+        }
+    });
+});
